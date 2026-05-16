f00f101289489472c86f58e6cce4190be6329d58
macien12 <160124196+macien12@users.noreply.github.com>
2026-05-16 18:38:21 +0200

    JS exercises completed from 06 to 11.
    All tests passed. Ready to commit changes.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+  return array.filter((item) => !args.includes(item));
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,14 @@
-const sumAll = function() {
+const sumAll = function(array1, array2) {
+    if (array1 < 0 || array2 < 0 || typeof array1 !== 'number' || typeof array2 !== 'number' || !Number.isInteger(array1) || !Number.isInteger(array2)) {
+        return 'ERROR';
+    }
+    let sum = 0;
+    let min = Math.min(array1, array2);
+    let max = Math.max(array1, array2);
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;     
 
 };
 
